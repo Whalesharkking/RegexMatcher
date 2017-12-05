4dbf9e96c790eee186387f7bb56a59d649f23aa1
Whalesharkking <elias_christen@gmx.ch>
2017-12-05 20:47:44 +0100

    init commit

---
+++ app/src/main/java/ch/christen/elias/regex_matcher/MainActivity.java
@@ -0,0 +1,13 @@
+package ch.christen.elias.regex_matcher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
